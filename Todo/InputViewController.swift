7a6da949c063b222c3a25b32e04658d5fe55fc48
AoiSakai <aoi_sakai@icloud.com>
2021-02-15 16:56:42 +0900

    [add]関連付け

---
+++ Todo/InputViewController.swift
@@ -0,0 +1,37 @@
+//
+//  InputViewController.swift
+//  Todo
+//
+//  Created by Sakai Aoi on 2021/02/15.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var deadlineDatePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func save() {
+    }
+    
+    @IBAction func cancel() {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func add() {
+    }
+    
 }
 
